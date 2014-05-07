6ce68e01ff440c4e77a07e5b8a17a46dd9d4c8f3
bwinslawski <wisla162@gmail.com>
2014-05-07 23:08:40 +0200

    nng
    ngnngn

--- Form.js
+++ Form.js
@@ -1,32 +1,33 @@
 (function($){
     $.fn.myPlugin = function(settings){
         var config = {
-                        'background-color': '#b0c4de'
+                        'color': '#D8D8D8'
         };
         if (settings){$.extend(config, settings);}
 		
 		function myFunction()
 	{     
-				 $.getJSON( "kods.json", function( data ) {
-			  var items = [];
-			  var multipleValues = $( "#kod" ).val()
-			 console.log("loo "+multipleValues);
-			  var l = 0;
-			  $.each( data, function( key, val ) {
-			  
-			  if (val["KOD POCZTOWY"]==multipleValues) {
-			  l=1;
-			 $( "#city" ).val(val["MIEJSCOW"]);
-			 $( "#kod" ).removeClass( "error" );
-				 }
-			  });
-			 if (l==0)
-			 {
-			 $( "#kod" ).addClass( "error" );
-			 }
-			 
-			  });
-			  
+			if($( "#kod" ).val()){
+						 $.getJSON( "kods.json", function( data ) {
+					  var items = [];
+					  var multipleValues = $( "#kod" ).val()
+					 console.log("loo "+multipleValues);
+					  var l = 0;
+					  $.each( data, function( key, val ) {
+					  
+					  if (val["KOD POCZTOWY"]==multipleValues) {
+					  l=1;
+					 $( "#city" ).val(val["MIEJSCOW"]);
+					 $( "#kod" ).removeClass( "error" );
+						 }
+					  });
+					 if (l==0)
+					 {
+					 $( "#kod" ).addClass( "error" );
+					 }
+					 
+					  });
+			  }
 	}
 	
 	
@@ -57,7 +58,7 @@
 				}
 				else {
 
-			  // console.log("kiki");
+			   
 				var numUpper = 0;
 				var numLower = 0;
 				var numNums = 0;
@@ -82,18 +83,23 @@
 				var inputElement = document.getElementById('password');
 				var errorElement = document.getElementById('passwordok');
 				var errorMsg="Wzorcowe hasło";
-				showMessageP(inputElement, errorMsg, errorElement);}
+				console.log("WZOROWR");
+				showMessageP(inputElement, errorMsg, errorElement);
+				}
 				else if (l == 2){
 				obj.result=false;
 				var inputElement = document.getElementById('password');
 				var errorElement = document.getElementById('passwordok');
 				var errorMsg="Średnie hasło";
-				showMessageP(inputElement, errorMsg, errorElement);}
+				console.log("WZOROWR1");
+				showMessageP(inputElement, errorMsg, errorElement);
+				}
 				else if(l<2){
 				obj.result=false;
 				var inputElement = document.getElementById('password');
 				var errorElement = document.getElementById('passwordok');
 				var errorMsg="Słabe hasło";
+				console.log("WZOROWR2");
 				showMessageP(inputElement, errorMsg, errorElement);
 				}
 				}
@@ -111,7 +117,7 @@
 				  }
 				  if (inputElement !== null) {
 					 inputElement.className = "passwordok";
-					 inputElement.focus();
+					
 				  }
 	}
 	
@@ -125,66 +131,66 @@
 			  }
 			  if (inputElement !== null) {
 				 inputElement.className = "error";
-				 inputElement.focus();
+				 
 			  }
-	}
+}
 	
     function validation()
     {
-	
-				$( "#kod" ).removeClass( "error" );
-				$( "#email" ).removeClass( "error" );
-				$( "#name" ).removeClass( "error" );
-				 var errorrElement = document.getElementById('emailError');
-					 errorrElement.innerHTML = " ";
-					 errorrElement = document.getElementById('nameError');
-					errorrElement.innerHTML = " ";
-					errorrElement = document.getElementById('passwordError');
-					errorrElement.innerHTML = " ";
-					errorrElement = document.getElementById('kodError');
-					errorrElement.innerHTML = " ";
+			
+			$( "#kod" ).removeClass( "error" );
+			$( "#email" ).removeClass( "error" );
+			$( "#name" ).removeClass( "error" );
+			 var errorrElement = document.getElementById('emailError');
+				 errorrElement.innerHTML = " ";
+				 errorrElement = document.getElementById('nameError');
+				errorrElement.innerHTML = " ";
+				errorrElement = document.getElementById('passwordError');
+				errorrElement.innerHTML = " ";
+				errorrElement = document.getElementById('kodError');
+				errorrElement.innerHTML = " ";
 
-				
-					var form =  document.getElementById('info-form'),
-						rules = { // 1
-							'name' : /^[a-z]{2,30} [a-z]{2,30}$/i,
-							'email': /^[a-z0-9\._%-]+@[a-z0-9\.-]+\.[a-z]{2,4}$/i,
-							'kod': /^[0-9]{2}\-[0-9]{3}$/,
-						};
-			 
-					for (var elem in rules) // 2
+			
+				var form =  document.getElementById('info-form'),
+					rules = { // 1
+						'name' : /^[a-z]{2,30} [a-z]{2,30}$/i,
+						'email': /^[a-z0-9\._%-]+@[a-z0-9\.-]+\.[a-z]{2,4}$/i,
+						'kod': /^[0-9]{2}\-[0-9]{3}$/,
+					};
+		 
+				for (var elem in rules) // 2
+				{
+					if (form[elem])
 					{
-						if (form[elem])
+						if (!rules[elem].test(form[elem].value)) // 3
 						{
-							if (!rules[elem].test(form[elem].value)) // 3
-							{
-								var inputElement = document.getElementById(elem);
-								var errorElement = document.getElementById(elem + "Error");
-								var errorMsg="nieprawidłowa wartość";
-								showMessage(inputElement, errorMsg, errorElement);
-							   // form[elem].style.background = 'red';
-								return false;
-							}
-							else
-							{
-								form[elem].style.background = '';
-							}
+							var inputElement = document.getElementById(elem);
+							var errorElement = document.getElementById(elem + "Error");
+							var errorMsg="nieprawidłowa wartość";
+							showMessage(inputElement, errorMsg, errorElement);
+						   // form[elem].style.background = 'red';
+							return false;
+						}
+						else
+						{
+							form[elem].style.background = '';
 						}
 					}
-					var haslo = document.getElementById('password').value
-					isOkPass(haslo);
-				 return false;
+				}
+				
+			 return false;
     }
 		
 		
 		
         return this.each(function(){
                 $(this).on('keydown', function(){
-				
+				$("#info-form").css("background-color",config.color);
 				
 				validation();
 				myFunction();
-				
+				var haslo = document.getElementById('password').value;
+				isOkPass(haslo);
 			           });
          });
     };
